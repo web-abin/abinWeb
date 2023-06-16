b8a059a03911545511c0850a3bb2596c60f0e5d5
abin <wellxabin@gmail.com>
2023-06-16 10:03:55 +0800

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,15 @@ export default defineNuxtConfig({
   app: {
     baseURL: isDev?'/':'/abinWeb/',
     head: {
+        title: '前端助手',
+        meta: [
+          { charset: 'utf-8' },
+          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+          { hid: 'description', name: 'description', content: 'My awesome app' }
+        ],
+        link: [
+          { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ],
         script: [
             { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js' },
             { src: 'https//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js' }
