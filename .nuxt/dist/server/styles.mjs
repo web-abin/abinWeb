54f243f0a3d57148bf4b7b5580201698b6cbaf46
王俊彬 <1872495742@qq.com>
2023-06-26 13:46:56 +0800

    test

--- .nuxt/dist/server/styles.mjs
+++ .nuxt/dist/server/styles.mjs
@@ -1,13 +1,13 @@
 const interopDefault = r => r.default || r || []
 export default {
-  "pages/404.vue": () => import('./nuxt/404-styles.95acde6c.mjs').then(interopDefault),
   "pages/demos/index.vue": () => import('./nuxt/index-styles.fd57ac60.mjs').then(interopDefault),
+  "pages/404.vue": () => import('./nuxt/404-styles.95acde6c.mjs').then(interopDefault),
   "pages/games/index.vue": () => import('./nuxt/index-styles.03ef7e63.mjs').then(interopDefault),
-  "pages/relax/components/fish.vue": () => import('./nuxt/fish-styles.21001825.mjs').then(interopDefault),
   "pages/links/index.vue": () => import('./nuxt/index-styles.5488edcc.mjs').then(interopDefault),
-  "pages/relax/index.vue": () => import('./nuxt/index-styles.f665abe1.mjs').then(interopDefault),
-  "pages/tools/index.vue": () => import('./nuxt/index-styles.8fbcb688.mjs').then(interopDefault),
   "pages/notes/index.vue": () => import('./nuxt/index-styles.c54da3c4.mjs').then(interopDefault),
+  "pages/relax/components/fish.vue": () => import('./nuxt/fish-styles.21001825.mjs').then(interopDefault),
+  "pages/relax/index.vue": () => import('./nuxt/index-styles.f27a985e.mjs').then(interopDefault),
+  "pages/tools/index.vue": () => import('./nuxt/index-styles.8fbcb688.mjs').then(interopDefault),
   "app.vue": () => import('./nuxt/app-styles.a91af329.mjs').then(interopDefault),
   "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": () => import('./nuxt/error-404-styles.80478c74.mjs').then(interopDefault),
   "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": () => import('./nuxt/error-500-styles.2483f26f.mjs').then(interopDefault),
