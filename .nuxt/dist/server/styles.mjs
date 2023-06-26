215d2435a1b8cceb830cf033785326b0efccb46f
王俊彬 <1872495742@qq.com>
2023-06-26 13:51:51 +0800

    test

--- .nuxt/dist/server/styles.mjs
+++ .nuxt/dist/server/styles.mjs
@@ -1,17 +1,17 @@
 const interopDefault = r => r.default || r || []
 export default {
+  "app.vue": () => import('./nuxt/app-styles.a91af329.mjs').then(interopDefault),
   "pages/demos/index.vue": () => import('./nuxt/index-styles.fd57ac60.mjs').then(interopDefault),
-  "pages/404.vue": () => import('./nuxt/404-styles.95acde6c.mjs').then(interopDefault),
   "pages/games/index.vue": () => import('./nuxt/index-styles.03ef7e63.mjs').then(interopDefault),
   "pages/links/index.vue": () => import('./nuxt/index-styles.5488edcc.mjs').then(interopDefault),
-  "pages/notes/index.vue": () => import('./nuxt/index-styles.c54da3c4.mjs').then(interopDefault),
   "pages/relax/components/fish.vue": () => import('./nuxt/fish-styles.21001825.mjs').then(interopDefault),
-  "pages/relax/index.vue": () => import('./nuxt/index-styles.f27a985e.mjs').then(interopDefault),
   "pages/tools/index.vue": () => import('./nuxt/index-styles.8fbcb688.mjs').then(interopDefault),
-  "app.vue": () => import('./nuxt/app-styles.a91af329.mjs').then(interopDefault),
+  "pages/404.vue": () => import('./nuxt/404-styles.95acde6c.mjs').then(interopDefault),
+  "pages/notes/index.vue": () => import('./nuxt/index-styles.c54da3c4.mjs').then(interopDefault),
+  "pages/relax/index.vue": () => import('./nuxt/index-styles.b7622f12.mjs').then(interopDefault),
   "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": () => import('./nuxt/error-404-styles.80478c74.mjs').then(interopDefault),
   "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": () => import('./nuxt/error-500-styles.2483f26f.mjs').then(interopDefault),
-  "pages/index.vue": () => import('./nuxt/index-styles.a06c7929.mjs').then(interopDefault),
   "layouts/default.vue": () => import('./nuxt/default-styles.9bd82805.mjs').then(interopDefault),
+  "pages/index.vue": () => import('./nuxt/index-styles.f35bd770.mjs').then(interopDefault),
   "components/Loading.vue": () => import('./nuxt/Loading-styles.ea7dbc13.mjs').then(interopDefault)
 }
