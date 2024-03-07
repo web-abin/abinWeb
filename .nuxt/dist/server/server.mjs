4c983bcd5735e6808d482325f35364533f06205e
王俊彬 <1872495742@qq.com>
2024-03-07 17:27:07 +0800

    feat

--- .nuxt/dist/server/server.mjs
+++ .nuxt/dist/server/server.mjs
@@ -493,7 +493,7 @@ const _routes = [
     meta: {},
     alias: [],
     redirect: void 0,
-    component: () => import("./static/index-19ceca2d.js").then((m) => m.default || m)
+    component: () => import("./static/index-a398c2a2.js").then((m) => m.default || m)
   },
   {
     name: "tools",
@@ -501,7 +501,7 @@ const _routes = [
     meta: {},
     alias: [],
     redirect: void 0,
-    component: () => import("./static/index-0b32f591.js").then((m) => m.default || m)
+    component: () => import("./static/index-967d1ce7.js").then((m) => m.default || m)
   },
   {
     name: "tools-tools",
@@ -509,7 +509,7 @@ const _routes = [
     meta: {},
     alias: [],
     redirect: void 0,
-    component: () => import("./static/tools-66f463d5.js").then((m) => m.default || m)
+    component: () => import("./static/tools-c3ba6948.js").then((m) => m.default || m)
   }
 ];
 const routerOptions0 = {
