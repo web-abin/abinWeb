1dcdb3f0feccd1e69cad053792d591669b0c83b3
王俊彬 <1872495742@qq.com>
2024-03-25 18:08:00 +0800

    feat

--- .nuxt/dist/server/server.mjs
+++ .nuxt/dist/server/server.mjs
@@ -493,7 +493,7 @@ const _routes = [
     meta: {},
     alias: [],
     redirect: void 0,
-    component: () => import("./static/index-5ac1c24a.js").then((m) => m.default || m)
+    component: () => import("./static/index-f9e1e001.js").then((m) => m.default || m)
   },
   {
     name: "tools",
