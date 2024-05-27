89e1c42cc79362fe2d91f7c48f1ef3d46e51f7e9
abin <wellxabin@gmail.com>
2024-05-27 17:49:06 +0800

    feat

--- .nuxt/types/nitro-routes.d.ts
+++ .nuxt/types/nitro-routes.d.ts
@@ -3,6 +3,9 @@ import type { Serialize, Simplify } from 'nitropack'
 declare module 'nitropack' {
   type Awaited<T> = T extends PromiseLike<infer U> ? Awaited<U> : T
   interface InternalApi {
+    '/__nuxt_error': {
+      'default': Simplify<Serialize<Awaited<ReturnType<typeof import('../../node_modules/.pnpm/nuxt@3.5.3_@types+node@18.16.18/node_modules/nuxt/dist/core/runtime/nitro/renderer').default>>>>
+    }
   }
 }
 export {}
