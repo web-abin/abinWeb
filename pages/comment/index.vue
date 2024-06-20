f22c7859bfe19d246760d5c435b00f36442b139e
abin <wellxabin@gmail.com>
2024-06-20 18:08:11 +0800

    feat

--- pages/comment/index.vue
+++ pages/comment/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="page">
+    <div class="loader" v-if="loadStatus === 1"></div>
+    <p class="error-tip" v-if="loadStatus === 3">评论功能加载失败</p>
     <div id="disqus_thread"></div>
     <noscript
       >Please enable JavaScript to view the
@@ -11,12 +13,18 @@
 </template>
 
 <script setup>
+const loadStatus = ref(1)
+
 onMounted(() => {
   var d = document,
     s = d.createElement('script')
   s.src = 'https://https-web-abin-github-io-abinweb-tools.disqus.com/embed.js'
   s.setAttribute('data-timestamp', +new Date())
   ;(d.head || d.body).appendChild(s)
+  loadStatus.value = 2
+  setTimeout(() => {
+    if (!document.querySelector('#disqus_thread iframe')) loadStatus.value = 3
+  }, 3000)
 })
 </script>
 
@@ -24,6 +32,53 @@ onMounted(() => {
 .page {
   min-height: calc(100vh - $--header-height - 160px);
   padding: 16px 64px;
+}
+.error-tip,
+.loader {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  margin: auto auto;
+}
+.error-tip {
+  width: fit-content;
+  height: fit-content;
+  color: #000;
+  font-size: 20px;
+  font-family: cursive, 'Courier New', Courier, monospace;
+}
+.loader {
+  width: 50px;
+  aspect-ratio: 1;
+  display: grid;
+  border-radius: 50%;
+  background: conic-gradient(
+    #25b09b 25%,
+    #f03355 0 50%,
+    #514b82 0 75%,
+    #ffa516 0
+  );
+  animation: l22 2s infinite linear;
+}
+.loader::before,
+.loader::after {
+  content: '';
+  grid-area: 1/1;
+  margin: 15%;
+  border-radius: 50%;
+  background: inherit;
+  animation: inherit;
+}
+.loader::after {
+  margin: 25%;
+  animation-duration: 3s;
+}
+@keyframes l22 {
+  100% {
+    transform: rotate(1turn);
+  }
 }
 </style>
 <style>
