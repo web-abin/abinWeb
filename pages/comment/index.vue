0a6620276184672bad33ac154d65bc8ec2ba6a29
王俊彬 <1872495742@qq.com>
2024-06-20 17:33:26 +0800

    feat

--- pages/comment/index.vue
+++ pages/comment/index.vue
@@ -11,15 +11,10 @@
 </template>
 
 <script setup>
-var disqus_config = function () {
-  this.page.url = PAGE_URL // Replace PAGE_URL with your page's canonical URL variable
-  this.page.identifier = PAGE_IDENTIFIER // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-}
 onMounted(() => {
-  // DON'T EDIT BELOW THIS LINE
   var d = document,
     s = d.createElement('script')
-  s.src = 'https://EXAMPLE.disqus.com/embed.js'
+  s.src = 'https://https-web-abin-github-io-abinweb-tools.disqus.com/embed.js'
   s.setAttribute('data-timestamp', +new Date())
   ;(d.head || d.body).appendChild(s)
 })
