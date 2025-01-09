1907205c68aca62b81057498e5055d0bcb350c87
王俊彬 <1872495742@qq.com>
2025-01-09 10:11:05 +0800

    feat

--- .nuxt/app.config.mjs
+++ .nuxt/app.config.mjs
@@ -1,5 +1,5 @@
 
-import { defuFn } from '/Users/wangjunbin/a-my-projects/github-abinweb/node_modules/defu/dist/defu.mjs'
+import { defuFn } from '/Users/wangjunbin/a-my-projects/github-abinweb/node_modules/.pnpm/defu@6.1.2/node_modules/defu/dist/defu.mjs'
 
 const inlineConfig = {}
 
