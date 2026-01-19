bd52686483ce1ee40dd9031dce07a70a1d6a9845
王俊彬 <junbin@cylingo.com>
2026-01-19 11:34:27 +0800

    feat

--- config.ts
+++ config.ts
@@ -2,8 +2,8 @@ export default {
   title: "前端导航-超好用",                                       // 网站标题
   author: "前端阿彬",                                           // 作者
   contact: "QQ:3532371314",                                        //联系方式
-  SEO_title: "前端助手 - 一站式前端开发资源导航与效率工具平台",                                        // 搜索引擎显示的标题
-  SEO_description: "前端助手是面向前端开发工程师的资源分享平台，通过工具。文档，游戏，摸鱼，案例等板块，建立一个完整的前端资源体系，提高前端开发者的开发效率，免费造福前端开发者的工具平台",               // description meta header
+  SEO_title: "前端助手 - 超好用的前端开发资源导航平台",                                        // 搜索引擎显示的标题
+  SEO_description: "超好用是面向前端开发工程师的资源分享平台，通过工具。文档，游戏，摸鱼，案例等板块，建立一个完整的前端资源体系，提高前端开发者的开发效率，免费造福前端开发者的工具平台",               // description meta header
   SEO_keywords: "前端工具, 编程导航, 前端导航, 前端开发手册, 程序员摸鱼, H5游戏集锦, 前端助手, 前端阿彬", // keywords meta header
   //工具页的description
   TOOLS_description: '前端助手工具页面：收录一些实用、可以提高开发效率的工具',
