e3f3cace3479b7376f1edfa9e8f7d38c73c4f9c5
abin <wellxabin@gmail.com>
2026-01-22 10:12:20 +0800

    feat

--- config.ts
+++ config.ts
@@ -1,16 +1,16 @@
 export default {
-  title: "前端导航-超好用",                                       // 网站标题
+  title: "前端助手-超好用的前端开发资源导航",                                       // 网站标题
   author: "前端阿彬",                                           // 作者
   contact: "QQ:3532371314",                                        //联系方式
-  SEO_title: "前端导航|超好用的前端开发资源导航平台",                                        // 搜索引擎显示的标题
-  SEO_description: "超好用是面向前端开发工程师的资源分享平台，通过工具。文档，游戏，摸鱼，案例等板块，建立一个完整的前端资源体系，提高前端开发者的开发效率，免费造福前端开发者的工具平台",               // description meta header
-  SEO_keywords: "前端工具, 编程导航, 前端导航, 前端开发手册, 程序员摸鱼, H5游戏集锦, 前端助手, 前端阿彬", // keywords meta header
+  SEO_title: "前端助手-超好用的前端开发资源导航平台",                                        // 搜索引擎显示的标题
+  SEO_description: "前端助手是面向前端开发工程师的资源分享平台，提供工具、文档、游戏、摸鱼、案例等板块，构建完整的前端资源体系，提升开发效率。",               // description meta header
+  SEO_keywords: "前端助手, 前端工具, 编程导航, 前端导航, 前端开发手册, 程序员摸鱼, H5游戏集锦, 前端阿彬", // keywords meta header
   //工具页的description
-  TOOLS_description: '前端助手工具页面：收录一些实用、可以提高开发效率的工具',
+  TOOLS_description: '前端助手工具页面：收录实用的前端开发工具与效率工具',
   //文档页的description
-  NOTES_description: '前端助手文档页面：收录前端开发可能用到的文档，方便查阅。提供 官方文档 和 相关文档 两种途径',
+  NOTES_description: '前端助手文档页面：收录前端开发常用文档，提供官方与相关文档入口',
   //游戏页的description
-  GAMES_description: '前端助手游戏页面：收录一些h5小游戏',
+  GAMES_description: '前端助手游戏页面：收录轻松休闲的 H5 小游戏',
   //摸鱼页的description
-  RELAX_description: '前端助手摸鱼页面：收录一些有趣的，有创意的网站地址，以后会陆续收录一些免费视频、音乐、或其他资源的网站',
+  RELAX_description: '前端助手摸鱼页面：收录有趣、有创意的网站，持续更新视频、音乐与资源类站点',
 };
